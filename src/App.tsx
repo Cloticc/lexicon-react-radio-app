0aa30e2d1e17ad51dfc132e234deda58c4e85d1d
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-28 21:44:32 +0100

    Add QueryClientProvider for React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 import { Channel } from "./components/Channel";
 import { Home } from './components/Home';
@@ -9,20 +13,27 @@ import { Navbar } from "./components/Navbar";
 import { ProgramComponent } from "./components/Program";
 import { ProgramDetails } from './components/ProgramDetails';
 
+const queryClient = new QueryClient()
+
 export function App() {
 
   return (
     <BrowserRouter>
       <Navbar />
       <LissenDirectly />
+        <QueryClientProvider client={queryClient}>
       <Routes>
-        <Route  path="/" element={<Home />} />
-        <Route path="/Channel" element={<Channel />} />
-        <Route path="/program/:id" element={<ProgramDetails />} /> 
-        <Route path="/Program" element={<ProgramComponent />} />
-        <Route path="/MyPage" element={<MyPage />} />
-        <Route path="/Login" element={<LoginForm />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/Channel" element={<Channel />} />
+          <Route path="/program/:id" element={<ProgramDetails />} />
+          <Route path="/Program" element={<ProgramComponent />} />
+          <Route path="/MyPage" element={<MyPage />} />
+          <Route path="/Login" element={<LoginForm />} />
       </Routes>
+        </QueryClientProvider>
     </BrowserRouter>
   );
 }
+
+
+
