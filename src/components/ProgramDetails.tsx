8f6353cacd4aba038cb686a363d75ce33701ffc8
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-28 22:41:04 +0100

    Refactor ProgramDetails component and add fetchPodFiles and fetchEpisodes functions

--- src/components/ProgramDetails.tsx
+++ src/components/ProgramDetails.tsx
@@ -1,9 +1,9 @@
 import 'react-tabs/style/react-tabs.css'; // Import the styles
 
+import { Episode, PodFile, Program } from '../interface/Interface';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 import { useEffect, useState } from 'react';
 
-import { Program } from '../interface/Interface';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 
@@ -29,18 +29,81 @@ export function ProgramDetails() {
     queryFn: fetchDetails
   });
 
-  if (isLoading) {
+
+
+
+  //   const fetchBroadCasts = async () => {
+  //     const response = await fetch(`https://api.sr.se/api/v2/broadcasts/?programid=${id}&format=json`);
+  //     if (!response.ok) {
+  //       throw new Error(`HTTP error! status: ${response.status}`);
+  //     }
+  //     const data = await response.json();
+  // console.log(data)
+  //     if (data) {
+  //       return data.broadcasts;
+  //     }
+  //     throw new Error('No broadcasts found');
+  //   }
+
+  //   const { data: broadcasts, isLoading: isLoadingBroadcasts, error: errorBroadcasts } = useQuery({
+  //     queryKey: ['broadcasts', id],
+  //     queryFn: fetchBroadCasts
+  //   });
+
+  const fetchPodFiles = async () => {
+    console.log(`Program ID: ${id}`); // Log the program ID
+    const response = await fetch(`https://api.sr.se/api/v2/podfiles?programid=${id}&format=json`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+
+    if (data) {
+      return data.podfiles;
+    }
+    throw new Error('No pod files found');
+  }
+
+  const { data: podFiles, isLoading: isLoadingPodFiles, error: errorPodFiles } = useQuery({
+    queryKey: ['podfiles', id],
+    queryFn: fetchPodFiles
+  });
+
+  const fetchEpisodes = async () => {
+    const today = new Date();
+    const todayStr = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
+console.log(`Today: ${todayStr}` );
+
+    const response = await fetch(`https://api.sr.se/api/v2/episodes/index?programid=${id}&fromdate=${todayStr}&todate=${todayStr}&format=json`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+
+    if (data) {
+      return data.episodes;
+    }
+    throw new Error('No episodes found');
+  }
+
+  const { data: episodes, isLoading: isLoadingEpisodes, error: errorEpisodes } = useQuery({
+    queryKey: ['episodes', id],
+    queryFn: fetchEpisodes
+  });
+
+
+
+  const isError = error || errorPodFiles || errorEpisodes;
+  const isLoadingAll = isLoading || isLoadingPodFiles || isLoadingEpisodes;
+
+  if (isLoadingAll) {
     return <p>Loading...</p>;
   }
 
-  if (error) {
-    return <p>Error: {error.message}</p>;
+  if (isError) {
+    return <p>Error</p>;
   }
-
-
-
-
-
 
   return (
     <div>
@@ -50,12 +113,13 @@ export function ProgramDetails() {
           <Tab>Details</Tab>
           <Tab>Broadcasts</Tab>
           <Tab>Pods</Tab>
-          <Tab>Old Chapters</Tab>
+          <Tab>episode</Tab>
         </TabList>
 
         <TabPanel>
           {/* Render details here */}
           <h1>{program?.name}</h1>
+          <p>{program?.broadcastinfo}</p>
           <p>{program?.description}</p>
           <img src={program?.programimage} alt={program?.name} />
           <a href={program?.programurl}>Visit Site</a>
@@ -65,14 +129,53 @@ export function ProgramDetails() {
         <TabPanel>
           {/* Render broadcasts here */}
           <h1>Test</h1>
+        </TabPanel>
+        <TabPanel>
+          <h1>Test</h1>
+
+          <ul>
+            {podFiles && podFiles.length > 0 ? (
+              podFiles.map((pod: PodFile) => (
+                <li key={pod.id}>
+                  <h2>{pod.title}</h2>
+                  <p>{pod.description}</p>
+                  <p>Duration: {pod.duration} seconds</p>
+                  <audio controls>
+                    <source src={pod.url} type="audio/mpeg" />
+                    Your browser does not support the audio element.
+                  </audio>
+                </li>
+              ))
+            ) : (
+              <p className='no-files'>No pod files available</p>
+            )}
+          </ul>
 
         </TabPanel>
         <TabPanel>
-          {/* Render pods here */}
+          {/* Render episode here */}
+          <h1>Test</h1>
+          <ul>
+            {episodes && episodes.length > 0 ? (
+              episodes.map((episode: Episode) => (
+                <li key={episode.id}>
+                  <h2>{episode.title}</h2>
+                  <p>{episode.description}</p>
+                  {/* <p>Duration: {episode.broadcast.timeend - episode.broadcast.timestart} seconds</p> */}
+                  <audio controls>
+                    <source src={episode.listenpodfile.url} type="audio/mpeg" />
+                    Your browser does not support the audio element.
+                  </audio>
+                </li>
+              ))
+            ) : (
+              <p className='no-files'>No episodes available</p>
+            )}
+          </ul>
+
+
         </TabPanel>
-        <TabPanel>
-          {/* Render old chapters here */}
-        </TabPanel>
+
       </Tabs>
     </div>
   );
