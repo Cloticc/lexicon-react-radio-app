4dac1ea4b62f5c543b6890edb09fa4807af744e6
Andreas Isidorsson <isidorssona@gmail.com>
2024-02-29 18:29:15 +0100

    Fix bug in login functionality

--- src/components/ProgramDetails.tsx
+++ src/components/ProgramDetails.tsx
@@ -8,31 +8,51 @@ import { useEpisodes, usePodFiles, useProgramDetails } from '../api/Episode';
 import { useParams } from 'react-router-dom';
 
 export function ProgramDetails() {
+
+
   const { id: idString } = useParams<{ id: string }>();
   const id = Number(idString);
-  const [page, setPage] = useState(1);
+  // const [page, setPage] = useState(1);
   const [tabIndex, setTabIndex] = useState(0);
 
   const { data: program, isLoading: programLoading, error: programError } = useProgramDetails(id);
-  const { data: podFiles, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading, isError, error } = usePodFiles(id);
+
 
   // const { isLoading: episodeLoading, error: episodeError } = useEpisodeDetails(id);
-  const { data: episodes, isLoading: episodesLoading, error: episodesError } = useEpisodes(id, page);
+  // const { data: episodes, isLoading: episodesLoading, error: episodesError } = useEpisodes(id, page);
 
 
-  const observer = useRef();
+  const { data: podFiles, fetchNextPage: fetchNextPodPage, hasNextPage: hasNextPodPage } = usePodFiles(id);
+  const { data: episodes, fetchNextPage: fetchNextEpisodePage, hasNextPage: hasNextEpisodePage } = useEpisodes(id);
 
-  const lastPodFileElementRef = useCallback(node => {
-    if (observer.current) observer.current.disconnect();
-    observer.current = new IntersectionObserver(entries => {
-      if (entries[0].isIntersecting && hasNextPage) {
-        fetchNextPage();
+
+
+  const episodeObserver = useRef<IntersectionObserver | null>(null);
+  const podObserver = useRef<IntersectionObserver | null>(null);
+
+  const lastEpisodeElementRef = useCallback((node: HTMLDivElement | null) => {
+    if (episodeObserver.current) episodeObserver.current.disconnect();
+    episodeObserver.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasNextEpisodePage) {
+        fetchNextEpisodePage();
       }
     });
-    if (node) observer.current.observe(node);
-  }, [fetchNextPage, hasNextPage]);
+    if (node) episodeObserver.current.observe(node);
+  }, [fetchNextEpisodePage, hasNextEpisodePage]);
 
+  const lastPodFileElementRef = useCallback((node: HTMLDivElement | null) => {
+    if (podObserver.current) podObserver.current.disconnect();
+    if (node) {
+      podObserver.current = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting && hasNextPodPage) {
+          fetchNextPodPage();
+        }
+      });
+      podObserver.current.observe(node);
+    }
+  }, [fetchNextPodPage, hasNextPodPage]
 
+  );
 
   useEffect(() => {
     if (localStorage.getItem('isRefreshed')) {
@@ -47,15 +67,15 @@ export function ProgramDetails() {
     };
   }, []);
 
-  if (programLoading || isLoading || episodesLoading) {
+  if (programLoading || !program) {
     return <div>Loading...</div>;
   }
 
-  if (programError || isError || episodesError) {
-    return <div>Error: {programError?.message || isError || episodesError?.message}</div>;
+  if (programError) {
+    return <div>Error: {programError?.message}</div>;
   }
 
-  console.log(`podFiles`, podFiles);
+  // console.log(`podFiles`, podFiles);
 
   // console.log('episodes', episodes);
   //   episodes.forEach((episode: any) => {
@@ -109,7 +129,7 @@ export function ProgramDetails() {
                       <div className='mb-4'>
                         <div className='text-gray-900 font-bold text-sm mb-1'>{pod.title}</div>
                         <p className='text-gray-700 text-xs'>{pod.description}</p>
-                        <p className='text-xs text-gray-500'>Duration: {pod.duration} seconds</p>
+                       <p className='text-xs text-gray-500'>Duration: {Math.round(pod.duration / 60)} minutes</p>
                       </div>
                       <div className='flex items-center'>
                         <audio controls>
@@ -131,43 +151,51 @@ export function ProgramDetails() {
           {/* Render episode here */}
           <div className='flex justify-center items-center'>
             <div className='grid grid-cols-1 sm:grid-cols-3 gap-4'>
-              {episodes && episodes.length > 0 ? (
-                episodes.map((episode: IEpisode) => (
-                  <div key={episode.id ? episode.id : 'No ID'} className='shadow-md rounded-lg overflow-hidden bg-white'>
-                    {episode.imageurl ? (
-                      <div
-                        className='h-48 bg-cover bg-center'
-                        style={{
-                          backgroundImage: `url(${episode.imageurl})`
-                        }}
-                        title={episode.title ? episode.title : 'No title available'}
-                      />
-                    ) : null}
-                    <div className='px-6 py-4'>
-                      <div className='font-bold text-xl mb-2'>{episode.title ? episode.title : 'No title available'}</div>
-                      <p className='text-gray-700 text-base'>{episode.description ? episode.description : 'No description available'}</p>
-                      <p className='text-sm text-gray-500 mt-2'>
-                        Published: {episode.publishdateutc ? new Date(Number(episode.publishdateutc.replace(/\/Date\((\d+)\)\//, '$1'))).toLocaleDateString('en-GB') : 'No publish date available'}
-                      </p>
-                    </div>
-                    {episode.broadcast ? (
-                      <div className='px-6 pb-4'>
-                        <audio controls src={episode.broadcast.broadcastfiles[0].url} className='w-full' />
+              {episodes && episodes.pages.length > 0 ? (
+                episodes.pages.map((page, pageIndex) => (
+                  page.data.map((episode: IEpisode, episodeIndex: number) => (
+                    <div
+                      key={episode.id ? episode.id : 'No ID'}
+                      ref={pageIndex === episodes.pages.length - 1 && episodeIndex === page.data.length - 1 ? lastEpisodeElementRef : null}
+                      className='shadow-md rounded-lg  bg-white'
+                    >
+                      <div key={episode.id ? episode.id : 'No ID'} className='shadow-md rounded-lg  bg-white'>
+                        {episode.imageurl ? (
+                          <div
+                            className='h-48 bg-cover bg-center'
+                            style={{
+                              backgroundImage: `url(${episode.imageurl})`
+                            }}
+                            title={episode.title ? episode.title : 'No title available'}
+                          />
+                        ) : null}
+                        <div className='px-6 py-4'>
+                          <div className='font-bold text-xl mb-2'>{episode.title ? episode.title : 'No title available'}</div>
+                          <p className='text-gray-700 text-base'>{episode.description ? episode.description : 'No description available'}</p>
+                          <p className='text-sm text-gray-500 mt-2'>
+                            Published: {episode.publishdateutc ? new Date(Number(episode.publishdateutc.replace(/\/Date\((\d+)\)\//, '$1'))).toLocaleDateString('en-GB') : 'No publish date available'}
+                          </p>
+                        </div>
+                        {episode.broadcast ? (
+                          <div className='px-6 pb-4'>
+                            <audio controls src={episode.broadcast.broadcastfiles[0].url} className='w-full' />
+                          </div>
+                        ) : null}
+                        <div className='px-6 py-4 flex justify-between'>
+                          {episode.url && episode.url ? (
+                            <a href={episode.url} className='text-blue-500'>URL</a>
+                          ) : (
+                            <p className='text-red-500'>No URL available</p>
+                          )}
+                          {episode.url && episode.url ? (
+                            <a href={episode.url} download className='text-blue-500'>Download</a>
+                          ) : (
+                            <p className='text-red-500'>No download file available</p>
+                          )}
+                        </div>
                       </div>
-                    ) : null}
-                    <div className='px-6 py-4 flex justify-between'>
-                      {episode.url && episode.url ? (
-                        <a href={episode.url} className='text-blue-500'>URL</a>
-                      ) : (
-                        <p className='text-red-500'>No URL available</p>
-                      )}
-                      {episode.url && episode.url ? (
-                        <a href={episode.url} download className='text-blue-500'>Download</a>
-                      ) : (
-                        <p className='text-red-500'>No download file available</p>
-                      )}
                     </div>
-                  </div>
+                  ))
                 ))
               ) : (
                 <p className='text-red-500 text-2xl'>No episodes available</p>
@@ -176,24 +204,6 @@ export function ProgramDetails() {
           </div>
 
 
-          <div className='flex justify-center mt-4'>
-            <button
-              type="button"
-              onClick={() => setPage(old => Math.max(old - 1, 1))}
-              disabled={page === 1}
-              className={`px-4 py-2 rounded-lg mr-2 ${page === 1 ? 'bg-gray-300 cursor-not-allowed text-gray-600' : 'bg-blue-500 text-white hover:bg-blue-700'}`}
-            >
-              Previous Page
-            </button>
-            <button
-              type="button"
-              onClick={() => setPage(old => old + 1)}
-              disabled={episodes && episodes.length === 0}
-              className={`px-4 py-2 rounded-lg ${episodes && episodes.length === 0 ? 'bg-gray-300 cursor-not-allowed text-gray-600' : 'bg-blue-500 text-white hover:bg-blue-700'}`}
-            >
-              Next Page
-            </button>
-          </div>
 
         </TabPanel>
 
