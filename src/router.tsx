3daa7d72be6de5518636e46616a29617fce5f9fc
Andreas Isidorsson <isidorssona@gmail.com>
2024-03-01 14:34:17 +0100

    Add Home component to router

--- src/router.tsx
+++ src/router.tsx
@@ -6,6 +6,7 @@ import {
 
 import { App } from "./App";
 import { Channel } from "./components/Channel";
+import { Home } from "./components/Home";
 import { LoginForm } from "./components/LoginForm";
 import { MyPage } from "./components/MyPage";
 import { ProgramComponent } from "./components/Program";
@@ -13,13 +14,14 @@ import { ProgramDetails } from "./components/ProgramDetails";
 
 export const Router = createBrowserRouter(
   createRoutesFromElements(
-      <Route path="/" element={<App />}>
-        <Route path="Channel" element={<Channel />} />
-        <Route path="program/:id" element={<ProgramDetails />} />
-        <Route path="Program" element={<ProgramComponent />} />
-        <Route path="MyPage" element={<MyPage />} />
-        <Route path="Login" element={<LoginForm />} />
-      </Route>
+    <Route path="/" element={<App />}>
+      <Route index path="/" element={<Home />} />
+      <Route path="Channel" element={<Channel />} />
+      <Route path="program/:id" element={<ProgramDetails />} />
+      <Route path="Program" element={<ProgramComponent />} />
+      <Route path="MyPage" element={<MyPage />} />
+      <Route path="Login" element={<LoginForm />} />
+    </Route>
 
   )
 );
