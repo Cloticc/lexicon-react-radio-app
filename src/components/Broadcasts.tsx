f38bd4c98a0995218fa8276fbe4080a2b864af7d
Andreas Isidorsson <isidorssona@gmail.com>
2024-03-01 22:34:48 +0100

    Refactor Broadcasts component to improve readability and remove console.log statements

--- src/components/Broadcasts.tsx
+++ src/components/Broadcasts.tsx
@@ -4,7 +4,7 @@ import { useBroadcasts } from '../api/apiBroadcasts';
 export const Broadcasts = () => {
   const { data: broadcasts, isLoading: broadcastLoading, error: broadcastError } = useBroadcasts();
 
-console.log(broadcasts);
+  // console.log(broadcasts);
 
 
 
@@ -21,15 +21,15 @@ console.log(broadcasts);
       <header className='header-wrapper'>
       </header>
       {broadcasts && broadcasts.map((broadcast: IBroadcast, index: number) => (
-  <div key={index} className="m-4 p-4 border rounded shadow-lg bg-white">
-    <h2 className="text-2xl font-bold mb-2">{broadcast.name}</h2>
-    <p className="text-gray-700">{broadcast.description}</p>
-    <p>Start Time: {new Date(parseInt(broadcast.localstarttime.replace(/[^0-9 +]/g, ''))).toLocaleString('eu', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false })}</p>
-    <p>Stop Time: {new Date(parseInt(broadcast.localstoptime.replace(/[^0-9 +]/g, ''))).toLocaleString('eu', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false })}</p>
-    <p>Channel: <a href={broadcast.liveaudio.url} target="_blank" rel="noopener noreferrer">{broadcast.channel.name}</a></p>
-    <p>Publisher: {broadcast.publisher.name}</p>
-  </div>
-))}
+        <div key={index} className="m-4 p-4 border rounded shadow-lg bg-white">
+          <h2 className="text-2xl font-bold mb-2">{broadcast.name}</h2>
+          <p className="text-gray-700">{broadcast.description}</p>
+          <p>Start Time: {new Date(parseInt(broadcast.localstarttime.replace(/[^0-9 +]/g, ''))).toLocaleString('eu', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false })}</p>
+          <p>Stop Time: {new Date(parseInt(broadcast.localstoptime.replace(/[^0-9 +]/g, ''))).toLocaleString('eu', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false })}</p>
+          <p>Channel: <a href={broadcast.liveaudio.url} target="_blank" rel="noopener noreferrer">{broadcast.channel.name}</a></p>
+          <p>Publisher: {broadcast.publisher.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
