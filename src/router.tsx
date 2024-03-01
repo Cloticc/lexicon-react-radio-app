8bc2d76e404fe8cf01a6984d6cc7246abaf222ca
Andreas Isidorsson <isidorssona@gmail.com>
2024-03-01 16:12:44 +0100

    Commented out unused imports and routes in router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,27 +1,30 @@
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
+// import {
+//   Route,
+//   createBrowserRouter,
+//   createRoutesFromElements,
+// } from "react-router-dom";
 
-import { App } from "./App";
-import { Channel } from "./components/Channel";
-import { Home } from "./components/Home";
-import { LoginForm } from "./components/LoginForm";
-import { MyPage } from "./components/MyPage";
-import { ProgramComponent } from "./components/Program";
-import { ProgramDetails } from "./components/ProgramDetails";
+// import { App } from "./App";
+// import { Channel } from "./components/Channel";
+// import { Home } from "./components/Home";
+// import { LoginForm } from "./components/LoginForm";
+// import { MyPage } from "./components/MyPage";
+// import NotFound from "./components/NotFound";
+// import { ProgramComponent } from "./components/Program";
+// import { ProgramDetails } from "./components/ProgramDetails";
 
-export const Router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index path="/" element={<Home />} />
-      <Route path="Channel" element={<Channel />} />
-      <Route path="Program" element={<ProgramComponent />} />
-      <Route path="program/:id" element={<ProgramDetails />} />
-      <Route path="MyPage" element={<MyPage />} />
-      <Route path="Login" element={<LoginForm />} />
-    </Route>
-
-  )
-);
+// export const Router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<App />}>
+//       <Route index path="/" element={<Home />} />
+//       <Route path="Channel" element={<Channel />} />
+//       <Route path="Program" element={<ProgramComponent />} />
+//       <Route path="program/:id" element={<ProgramDetails />} />
+//       <Route path="MyPage" element={<MyPage />} />
+//       <Route path="Login" element={<LoginForm />} />
+//       <Route path="*" element={<NotFound />} />
+//     </Route>
+     
+ 
+//   )
+// );
