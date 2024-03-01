de1958829972f51a12fb2d3393ac0ded47182441
Andreas Isidorsson <isidorssona@gmail.com>
2024-03-01 20:08:26 +0100

    Refactor Channel component and implement pagination logic

--- src/components/Channel.tsx
+++ src/components/Channel.tsx
@@ -1,159 +1,71 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 
 import { IChannel } from '../interface/Interface';
-import { fetchData } from '../api/fetchData';
+import { Link } from 'react-router-dom';
+import { useChannel } from '../api/apiChannel';
 
-export const Channel: React.FC = () => {
-  const [channels, setChannels] = useState<IChannel[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-  const [page, setPage] = useState(1);
-
+export const Channel = () => {
+  const [playingAudio, setPlayingAudio] = useState<HTMLAudioElement | null>(null);
+  const { data: channels, isLoading, isError } = useChannel(); // use the hook
   const observer = useRef<IntersectionObserver | null>(null);
-  const lastChannelElementRef = useCallback((node: HTMLLIElement | null) => {
+  const lastChannelElementRef = useCallback((node: HTMLDivElement | null) => {
     if (isLoading) return;
     if (observer.current) observer.current.disconnect();
     observer.current = new IntersectionObserver(entries => {
       if (entries[0].isIntersecting) {
-        setPage(prevPage => prevPage + 1);
+        // TODO: Implement pagination logic here
       }
     });
     if (node) observer.current.observe(node);
   }, [isLoading]);
 
-  useEffect(() => {
-    const fetchChannels = async () => {
-      const { data, isLoading, error } = await fetchData(`channels?page=${page}&format=json`);
 
-      if (data) {
-        setChannels(prevChannels => [...prevChannels, ...data.channels]);
-      } else {
-        setError('No channels found');
-      }
-
-      setIsLoading(isLoading);
-      if (error) {
-        setError(error);
-      }
-    };
-
-    fetchChannels();
-  }, [page]);
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
-  if (error) {
-    return <div>Error: {error}</div>;
+  if (isError) {
+    return <div>Error fetching channels</div>;
   }
 
   return (
-    <div className="channel">
-      <h1>Kanaler</h1>
-      <ul>
-        {channels.map((channel: IChannel, index: number) => (
-          <li ref={index === channels.length - 1 ? lastChannelElementRef : null} key={`${channel.id}-${channel.name}-${index}`}>
-            <h2>{channel.name}</h2>
-            <img src={channel.image} alt={channel.name} />
-            <p>{channel.tagline}</p>
-            <a href={channel.siteurl}>Visit Site</a>
+    <div className="channel grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+      <h1 className="col-span-full text-2xl mb-4">Channel</h1>
+      {channels?.map((channel: IChannel, index: number) => (
+        <div ref={index === channels.length - 1 ? lastChannelElementRef : null} key={`${channel.id}-${channel.name}-${index}`} className="card border rounded-lg overflow-hidden shadow-md">
+          <img src={channel.image} alt={channel.name} className="w-full h-64 object-contain" />
+          <div className="p-4">
+            <h2 className="text-xl mb-2">{channel.name}</h2>
+            <p className="text-gray-700 mb-2">{channel.tagline}</p>
+            <a href={channel.siteurl} className="text-blue-500 hover:underline mb-2 block">Visit Site</a>
             <div>
-              <h3>Live Audio</h3>
-              <audio controls src={channel.liveaudio.url}>
+              <h3 className="text-lg mb-2">Live Audio</h3>
+              <audio
+                controls
+                src={channel.liveaudio.url}
+                className="w-full"
+                onPlay={(event) => {
+                  const audio = event.currentTarget;
+                  if (playingAudio && playingAudio !== audio) {
+                    playingAudio.pause();
+                  }
+                  setPlayingAudio(audio);
+                }}
+              >
                 Your browser does not support the audio element.
               </audio>
             </div>
-            <a href={channel.scheduleurl}>Schedule</a>
-            <p>{channel.channeltype}</p>
-            <p>{channel.xmltvid}</p>
-          </li>
-        ))}
-      </ul>
+            <a href={channel.scheduleurl} className="text-blue-500 hover:underline mb-2 block">Schedule</a>
+            <p className="text-gray-700 mb-2">{channel.channeltype}</p>
+            <p className="text-gray-700">{channel.xmltvid}</p>
+
+            <Link className="text-blue-500 hover:underline" to={`/Program/${channel.id}`}>
+              Jump to details
+            </Link>
+          </div>
+        </div>
+      ))}
     </div>
   );
-}
-
-
-//Old code used for fetching XML data instead of JSON.
-
-// import '../styles/Channel.css';
-
-// import { useEffect, useState } from 'react';
-
-// import { fetchData } from '../api/fetchData';
-
-// interface Channel {
-//   id: string;
-//   name: string;
-//   image: string;
-//   tagline: string;
-
-// }
-
-// export const Channel: React.FC = () => {
-//   const [channels, setChannels] = useState<Channel[]>([]);
-//   const [isLoading, setIsLoading] = useState(true);
-//   const [error, setError] = useState<string | null>(null);
-
-//   useEffect(() => {
-//     const fetchChannels = async () => {
-//       const { data, isLoading, error } = await fetchData('channels');
-
-//       if (data) {
-//         const channelNodes = data.getElementsByTagName('channel');
-//         const parsedChannels = Array.from(channelNodes).map((channelNode) => {
-//           return {
-//             id: channelNode.getAttribute('id') || '',
-//             name: channelNode.getElementsByTagName('name')[0]?.textContent || '',
-//             image: channelNode.getElementsByTagName('image')[0]?.textContent || '',
-//             color: channelNode.getElementsByTagName('color')[0]?.textContent || '',
-//             tagline: channelNode.getElementsByTagName('tagline')[0]?.textContent || '',
-//             siteurl: channelNode.getElementsByTagName('siteurl')[0]?.textContent || '',
-//             liveaudio: {
-//               id: channelNode.getElementsByTagName('liveaudio')[0]?.getAttribute('id') || '',
-//               url: channelNode.getElementsByTagName('url')[0]?.textContent || '',
-//               statkey: channelNode.getElementsByTagName('statkey')[0]?.textContent || '',
-//             },
-//             scheduleurl: channelNode.getElementsByTagName('scheduleurl')[0]?.textContent || '',
-//             channeltype: channelNode.getElementsByTagName('channeltype')[0]?.textContent || '',
-//             xmltvid: channelNode.getElementsByTagName('xmltvid')[0]?.textContent || '',
-//           };
-//         });
-//         setChannels(parsedChannels);
-//       } else {
-//         setError('No channels found');
-//       }
-
-//       setIsLoading(isLoading);
-//       if (error) {
-//         setError(error);
-//       }
-//     };
-
-//     fetchChannels();
-//   }, []);
-
-//   if (isLoading) {
-//     return <div>Loading...</div>;
-//   }
-
-//   if (error) {
-//     return <div>Error: {error}</div>;
-//   }
-
-//   return (
-//     <div className="channel">
-//       <h1>Kanaler</h1>
-//       <ul>
-//         {channels.map((channel) => (
-//           <li key={channel.id}>
-//             <h2>{channel.name}</h2>
-//             <img src={channel.image} alt={channel.name} />
-//             <p>{channel.tagline}</p>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
+};
