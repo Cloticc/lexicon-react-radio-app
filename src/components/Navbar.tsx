277841b2741163d4c4ec39dff9267d5cbc468398
Andreas Isidorsson <isidorssona@gmail.com>
2024-03-02 17:47:35 +0100

    Update Navbar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ export function Navbar() {
   const [search, setSearch] = useState('');
 
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-gray-500 p-6">
+    <nav className="flex items-center justify-between flex-wrap bg-blue-600 p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <img src="https://upload.wikimedia.org/wikipedia/commons/d/d2/Logo-de-World-Hits-Radio.png" alt="Radio logo" className="h-8 w-8 mr-2" />
         <span className="font-semibold text-xl tracking-tight">Swedish Radio</span>
@@ -21,7 +21,7 @@ export function Navbar() {
       <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div className="text-sm lg:flex-grow">
           {links.map((link) => (
-            <Link to={link.to} className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-blue-500 mr-4" key={link.to}>
+            <Link to={link.to} className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-blue-300 mr-4" key={link.to}>
               {link.label}
             </Link>
           ))}
@@ -30,7 +30,7 @@ export function Navbar() {
           <input
             type="text"
             placeholder="Search Channel"
-            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-teal-500"
+            className="bg-white appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
             value={search}
             onChange={(event) => setSearch(event.target.value)}
           />
