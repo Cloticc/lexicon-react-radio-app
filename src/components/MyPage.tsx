c6ae0c6daf884b8b5bace3674ac2130480e87ee5
Andreas Isidorsson <isidorssona@gmail.com>
2024-03-02 17:47:33 +0100

    Update MyPage component with favorite tabs

--- src/components/MyPage.tsx
+++ src/components/MyPage.tsx
@@ -1,10 +1,30 @@
+import 'react-tabs/style/react-tabs.css'; // Import the styles
 
+import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 
 export const MyPage = () => {
   return (
     <div className="myPage">
-      <h1>Min Sida</h1>
-      <p>Logga in för att se din sida</p>
+      <h1>Favorit Page</h1>
+
+
+      <Tabs>
+        <TabList>
+          <Tab>Favorite Channel</Tab>
+          <Tab>Favorite program</Tab>
+        </TabList>
+        <TabPanel>
+          <h2>Favorite Channel</h2>
+          <p>No Favorite Channel</p>
+        </TabPanel>
+        <TabPanel>
+          <h2>Favorite program</h2>
+          <p>No Favorite program</p>
+        </TabPanel>
+      </Tabs>
     </div>
+
+
+
   )
 }
