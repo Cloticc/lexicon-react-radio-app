e4e9dc18bfd4e14864bb08a73022bdd3f6f3fd3d
Andreas Isidorsson <isidorssona@gmail.com>
2024-03-03 11:13:00 +0100

    Refactor ChannelDetails component to use Tabs

--- src/components/ChannelDetails.tsx
+++ src/components/ChannelDetails.tsx
@@ -25,48 +25,42 @@ export const ChannelDetails = () => {
 
 
   return (
-    <div className="ProgramDetails">
-      <h2>Program Details</h2>
-      <Tabs>
-        <TabList>
-          <Tab>BroadCasts</Tab>
-          <Tab>Program</Tab>
-        </TabList>
-        <TabPanel>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4">
-            {searchEpisode?.sort((a: { publishdateutc: string; }, b: { publishdateutc: string; }) => parseInt(b.publishdateutc.substring(6, b.publishdateutc.length - 2)) - parseInt(a.publishdateutc.substring(6, a.publishdateutc.length - 2))).map((episode: ISearchEpisode) => {
-              return (
-                <div key={episode.id} className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-auto">
-                  <div>
-                    <img className="h-48 w-full object-cover" src={episode.imageurl} alt={episode.title} />
-                  </div>
-                  <div className="p-8">
-                    <h3 className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{episode.title}</h3>
-                    <p className="mt-2 text-gray-500">{episode.description}</p>
-                    <p className="mt-2 text-gray-500">
-                      Published on: {new Date(parseInt(episode.publishdateutc.substring(6, episode.publishdateutc.length - 2))).toLocaleDateString('en-GB')}
-                    </p>
-                    <p>
-                      Broadcast Time: {new Date(parseInt(episode.broadcasttime.starttimeutc.substring(6, episode.broadcasttime.starttimeutc.length - 2))).toLocaleTimeString('en-GB')} - {new Date(parseInt(episode.broadcasttime.endtimeutc.substring(6, episode.broadcasttime.endtimeutc.length - 2))).toLocaleTimeString('en-GB')}
-                    </p>
-                    <a href={episode.url} className="mt-2 text-indigo-500 hover:underline">{episode.program.name} Link</a>
-                  </div>
+
+    <Tabs className="flex flex-col">
+      <TabList className="flex mb-4">
+        <Tab className="mr-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110">BroadCasts</Tab>
+        <Tab className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110">Program</Tab>
+      </TabList>
+      <TabPanel>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4">
+          {searchEpisode?.sort((a: { publishdateutc: string; }, b: { publishdateutc: string; }) => parseInt(b.publishdateutc.substring(6, b.publishdateutc.length - 2)) - parseInt(a.publishdateutc.substring(6, a.publishdateutc.length - 2))).map((episode: ISearchEpisode) => {
+            return (
+              <div key={episode.id} className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-auto">
+                <div>
+                  <img className="h-48 w-full object-cover" src={episode.imageurl} alt={episode.title} />
                 </div>
-              );
-            })}
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <h2>Program</h2>
-          <p>Program</p>
-        </TabPanel>
+                <div className="p-8">
+                  <h3 className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{episode.title}</h3>
+                  <p className="mt-2 text-gray-500">{episode.description}</p>
+                  <p className="mt-2 text-gray-500">
+                    Published on: {new Date(parseInt(episode.publishdateutc.substring(6, episode.publishdateutc.length - 2))).toLocaleDateString('en-GB')}
+                  </p>
+                  <p>
+                    Broadcast Time: {new Date(parseInt(episode.broadcasttime.starttimeutc.substring(6, episode.broadcasttime.starttimeutc.length - 2))).toLocaleTimeString('en-GB')} - {new Date(parseInt(episode.broadcasttime.endtimeutc.substring(6, episode.broadcasttime.endtimeutc.length - 2))).toLocaleTimeString('en-GB')}
+                  </p>
+                  <a href={episode.url} className="mt-2 text-indigo-500 hover:underline">{episode.program.name} Link</a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </TabPanel>
+      <TabPanel>
+        <h2 className="text-xl font-bold mb-2">Program</h2>
+        <p className="text-gray-700">Program</p>
+      </TabPanel>
+    </Tabs>
 
-
-
-
-
-      </Tabs>
-    </div>
   );
 }
 
