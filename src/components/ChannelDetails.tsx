7060a2409550abd7396af6777527ae4b1336fa90
Andreas Isidorsson <isidorssona@gmail.com>
2024-03-03 12:48:48 +0100

    Add program related to the channel in ChannelDetails component

--- src/components/ChannelDetails.tsx
+++ src/components/ChannelDetails.tsx
@@ -1,22 +1,25 @@
+import { IProgram, ISearchEpisode } from "../interface/Interface";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 
-import { ISearchEpisode } from "../interface/Interface";
 import { useParams } from "react-router-dom";
+import { useProgram } from "../api/apiProgram";
 import { useSearchEpisodes } from "../api/apiEpisode";
 
 export const ChannelDetails = () => {
 
-  const { name } = useParams<{ name: string }>();
+  const { name, channelId } = useParams();
 
   const { data: searchEpisode, isLoading: searchEpisodeLoading, error: searchEpisodeError } = useSearchEpisodes(name || "");
 
+  const { data: programs, isLoading: programsLoading, error: programsError } = useProgram(channelId ? parseInt(channelId) : 0);
+  console.log(channelId);
 
 
-  if (searchEpisodeLoading) {
+  if (searchEpisodeLoading || programsLoading) {
     return <div>Loading...</div>;
   }
 
-  if (searchEpisodeError) {
+  if (searchEpisodeError || programsError) {
     return <div>Error fetching data</div>;
   }
 
@@ -41,12 +44,12 @@ export const ChannelDetails = () => {
                   <h3 className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{episode.title}</h3>
                   <p className="mt-2 text-gray-500">{episode.description}</p>
                   <p className="mt-2 text-gray-500">
-                    Published on: {new Date(parseInt(episode.publishdateutc.substring(6, episode.publishdateutc.length - 2))).toLocaleDateString('en-GB')}
+                    Published on: {new Date(parseInt(episode.publishdateutc?.substring(6, episode.publishdateutc.length - 2))).toLocaleDateString('en-GB')}
                   </p>
                   <p>
-                    Broadcast Time: {new Date(parseInt(episode.broadcasttime.starttimeutc.substring(6, episode.broadcasttime.starttimeutc.length - 2))).toLocaleTimeString('en-GB')} - {new Date(parseInt(episode.broadcasttime.endtimeutc.substring(6, episode.broadcasttime.endtimeutc.length - 2))).toLocaleTimeString('en-GB')}
+                    Broadcast Time: {new Date(parseInt(episode.broadcasttime?.starttimeutc.substring(6, episode.broadcasttime.starttimeutc.length - 2))).toLocaleTimeString('en-GB')} - {new Date(parseInt(episode.broadcasttime?.endtimeutc.substring(6, episode.broadcasttime.endtimeutc.length - 2))).toLocaleTimeString('en-GB')}
                   </p>
-                  <a href={episode.url} className="mt-2 text-indigo-500 hover:underline">{episode.program.name} Link</a>
+                  <a href={episode.url} className="mt-2 text-indigo-500 hover:underline">{episode.program?.name} Link</a>
                 </div>
               </div>
             );
@@ -54,7 +57,23 @@ export const ChannelDetails = () => {
         </div>
       </TabPanel>
       <TabPanel>
-        <h2 className="text-xl font-bold mb-2">Program</h2>
+        {/* program related to the channel */}
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-4">
+          {programs?.map((program: IProgram) => {
+            return (
+              <div key={program.id} className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-auto">
+                <div>
+                  <img className="h-48 w-full object-cover" src={program.programimagetemplate.replace("{?}", "400x400")} alt={program.name} />
+                </div>
+                <div className="p-8">
+                  <h3 className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{program.name}</h3>
+                  <p className="mt-2 text-gray-500">{program.description}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
 
       </TabPanel>
     </Tabs>
