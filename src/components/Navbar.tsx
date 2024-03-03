a7ade1e0248fe00e9890e08584f940ba56ccb8c9
Andreas Isidorsson <isidorssona@gmail.com>
2024-03-03 13:33:21 +0100

    Refactor Navbar component styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,15 +13,15 @@ export function Navbar() {
   const [search, setSearch] = useState('');
 
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-blue-600 p-6">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/d/d2/Logo-de-World-Hits-Radio.png" alt="Radio logo" className="h-8 w-8 mr-2" />
-        <span className="font-semibold text-xl tracking-tight">Swedish Radio</span>
+    <nav className="bg-blue-600 p-6 flex items-center justify-between">
+      <div className="flex items-center space-x-2">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/d/d2/Logo-de-World-Hits-Radio.png" alt="Radio logo" className="h-8 w-8" />
+        <span className="text-white text-xl font-semibold tracking-tight">Swedish Radio</span>
       </div>
-      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div className="text-sm lg:flex-grow">
+      <div className="flex items-center space-x-4">
+        <div className="space-x-4">
           {links.map((link) => (
-            <Link to={link.to} className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-blue-300 mr-4" key={link.to}>
+            <Link to={link.to} className="text-white hover:text-blue-300" key={link.to}>
               {link.label}
             </Link>
           ))}
@@ -30,7 +30,7 @@ export function Navbar() {
           <input
             type="text"
             placeholder="Search Channel"
-            className="bg-white appearance-none border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+            className="rounded py-2 px-4 text-gray-700 bg-white border-2 border-gray-200 focus:outline-none focus:bg-white focus:border-blue-500"
             value={search}
             onChange={(event) => setSearch(event.target.value)}
           />
