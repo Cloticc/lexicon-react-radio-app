b8a336e9fdf0a236250d64bc3d5eba5460bd5d31
Andreas Isidorsson <isidorssona@gmail.com>
2024-03-04 14:31:12 +0100

    Update heading text in MyPage component

--- src/components/MyPage.tsx
+++ src/components/MyPage.tsx
@@ -14,7 +14,7 @@ export const MyPage = () => {
 
   return (
     <div className="myPage bg-gray-100 p-4 rounded-md shadow-lg">
-      <h1 className="text-2xl font-bold mb-4">Favorite Page</h1>
+      <h1 className="text-2xl font-bold mb-4">Favoriter</h1>
       <Tabs>
         <TabList className="flex border-b">
           <Tab className="mr-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110">Favorit kanal</Tab>
