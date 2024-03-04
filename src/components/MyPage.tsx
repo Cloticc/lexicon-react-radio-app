80fe7a57043b63881f4c8b65810c9621987cb71c
Andreas Isidorsson <isidorssona@gmail.com>
2024-03-04 16:31:31 +0100

    Update favorite button color in MyPage component

--- src/components/MyPage.tsx
+++ src/components/MyPage.tsx
@@ -45,7 +45,7 @@ export const MyPage = () => {
                         <h2 className="text-xl font-bold">Länk till detaljer</h2>
                       </Link>
                       <button onClick={() => removeFavorite(channel)} className="absolute top-0 right-0 m-2">
-                        <FontAwesomeIcon icon={faStar} />
+                        <FontAwesomeIcon icon={faStar} color="red"/>
                       </button>
                     </div>
                   </div>
@@ -62,19 +62,19 @@ export const MyPage = () => {
                 const program = favorite as IProgram;
                 return (
                   <div key={program.id} className="card border rounded-lg overflow-hidden shadow-md bg-white relative">
-                    <img src={program.programimage} alt={program.name} className="w-full h-64 object-cover-fill" />
-                    <div className="p-4">
-                      <h2 className="text-xl mb-2">{program.name}</h2>
-                      <p className="text-gray-700 mb-2">{program.description}</p>
-                      <p className="text-gray-700 mb-2">{program.broadcastinfo}</p>
-                      <a href={program.programurl} className="text-blue-500 hover:underline mb-2 block">Besök sida</a>
-                      <Link className="text-blue-500 hover:underline" to={`/programs/program/${program.id}`}>
-                        <h2 className="text-xl font-bold">Länk till detaljer</h2>
-                      </Link>
-                      <button onClick={() => removeFavorite(program)} className="absolute top-0 right-0 m-2">
-                        <FontAwesomeIcon icon={faStar} />
-                      </button>
-                    </div>
+                  <img src={program.programimage} alt={program.name} className="w-full h-64 object-cover-fill" />
+                  <div className="p-4">
+                    <h2 className="text-xl mb-2">{program.name}</h2>
+                    <p className="text-gray-700 mb-2">{program.description}</p>
+                    <p className="text-gray-700 mb-2">{program.broadcastinfo}</p>
+                    <a href={program.programurl} className="text-blue-500 hover:underline mb-2 block">Besök sida</a>
+                    <Link className="text-blue-500 hover:underline" to={`/programs/program/${program.id}`}>
+                    <h2 className="text-xl font-bold">Länk till detaljer</h2>
+                    </Link>
+                    <button onClick={() => removeFavorite(program)} className="absolute top-0 right-0 m-2">
+                    <FontAwesomeIcon icon={faStar} color="red" />
+                    </button>
+                  </div>
                   </div>
                 );
               }
