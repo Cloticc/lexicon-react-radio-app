0a103f58bf60a0cd6cdd8fccd4f63fb809a9d3a1
Andreas Isidorsson <isidorssona@gmail.com>
2024-03-04 14:07:40 +0100

    Add FontAwesomeIcon and faStar to MyPage component

--- src/components/MyPage.tsx
+++ src/components/MyPage.tsx
@@ -3,8 +3,10 @@ import 'react-tabs/style/react-tabs.css';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 
 import { FavoriteContext } from '../context/ContexProvider'; // Adjust the path to match your project structure
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { IChannel } from '../interface/Interface';
 import { Link } from 'react-router-dom';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { useContext } from 'react';
 
 export const MyPage = () => {
@@ -19,30 +21,32 @@ export const MyPage = () => {
           <Tab className="mr-1 py-2 px-4 font-semibold text-gray-800 bg-white rounded-t-lg">Favorite program</Tab>
         </TabList>
         <TabPanel className="p-4 bg-white rounded-b-lg">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {favorites.map((favorite: IChannel) => (
-          <div key={favorite.id} className="rounded overflow-hidden shadow-lg m-2 bg-white">
-            <img className="w-full h-64 object-cover" src={favorite.image} alt={favorite.name} />
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">{favorite.name}</div>
-              <p className="text-gray-700 text-base">{favorite.tagline}</p>
-            </div>
-            <div className="px-6 pt-4 pb-2">
-              {/* <a href={favorite.siteurl} className="inline-block bg-blue-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Visit Site</a>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {favorites.map((favorite: IChannel) => (
+              <div key={favorite.id} className="relative rounded overflow-hidden shadow-lg m-2 bg-white">
+                <img className="w-full h-64 object-cover" src={favorite.image} alt={favorite.name} />
+                <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">{favorite.name}</div>
+                  <p className="text-gray-700 text-base">{favorite.tagline}</p>
+                </div>
+                <div className="px-6 pt-4 pb-2">
+                  {/* <a href={favorite.siteurl} className="inline-block bg-blue-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Visit Site</a>
               <Link className="inline-block bg-blue-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2" to={`/channels/favorite/${favorite.name}/${favorite.id}`}>
                 Link to details
               </Link> */}
-                 <a href={favorite.siteurl} className="text-blue-500 hover:underline mb-2 block">Visit Site</a>
-            <Link className="text-blue-500 hover:underline" to={`/channels/channel/${favorite.name}/${favorite.id}`}>
-              <h2 className="text-xl font-bold">Link to details</h2>
-            </Link>
-            </div>
-            <div className="px-6 pt-4 pb-2">
-              <button onClick={() => removeFavorite(favorite)} className="inline-block bg-red-500 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2 mb-2">Remove</button>
-            </div>
+                  <a href={favorite.siteurl} className="text-blue-500 hover:underline mb-2 block">Visit Site</a>
+                  <Link className="text-blue-500 hover:underline" to={`/channels/channel/${favorite.name}/${favorite.id}`}>
+                    <h2 className="text-xl font-bold">Link to details</h2>
+                  </Link>
+                </div>
+                <div className="absolute top-0 right-0 m-2">
+                  <button onClick={() => removeFavorite(favorite)} className="absolute top-0 right-0 m-2 ">
+                    <FontAwesomeIcon icon={faStar} />
+                  </button>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
         </TabPanel>
         <TabPanel className="p-4 bg-white rounded-b-lg">
           <h2 className="text-xl font-semibold mb-2">Favorite program</h2>
