13707fe625659560b3315a86f3782f7db0e3c219
Andreas Isidorsson <isidorssona@gmail.com>
2024-03-04 16:28:02 +0100

    Refactor ProgramComponent rendering

--- src/components/Program.tsx
+++ src/components/Program.tsx
@@ -73,11 +73,11 @@ export const ProgramComponent: React.FC = () => {
           </div>
         </div>
       </div>
-      
+
       <div className='container mx-auto p-4'>
         <aside className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
           {programs?.map((program, index) => (
-            <li key={`${program.id}-${index}`} className='border rounded-lg shadow-lg bg-white list-none p-4 space-y-4'>
+            <li key={`${program.id}-${index}`} className='relative border rounded-lg shadow-lg bg-white list-none p-4 space-y-4'>
               <img className="w-full h-64 object-cover mt-2 rounded" src={program.programimage} alt={program.name} />
               <div className="space-y-2">
                 <h3 className="text-lg font-semibold">{program.name}</h3>
@@ -93,9 +93,9 @@ export const ProgramComponent: React.FC = () => {
               <Link className="text-blue-500 hover:underline" to={`/programs/program/${program.id}`}>
                 <h2 className="text-xl font-bold">Länk till detaljer</h2>
               </Link>
-              <button onClick={() => addFavorite(program)} className="absolute top-0 right-0 m-2">
-              <FontAwesomeIcon icon={faStar} />
-            </button>
+              <button onClick={() => addFavorite({ ...program, type: 'program' })} className="absolute top-0 right-0 m-2">
+                <FontAwesomeIcon icon={faStar} />
+              </button>
             </li>
           ))}
         </aside>
