cf0097339aa924c41b22c2ece46346bf0a8ad80c
Andreas Isidorsson <isidorssona@gmail.com>
2024-03-05 11:00:27 +0100

    Refactor search component to use programs instead of episodes

--- src/components/SearchComponent.tsx
+++ src/components/SearchComponent.tsx
@@ -1,58 +1,49 @@
-// SearchComponent.tsx
+// import { IProgram } from '../interface/Interface';
 
-import { IEpisode } from '../interface/Interface';
+import { IProgram } from '../interface/Interface';
 import React from 'react';
-import { useSearchEpisodes } from '../api/apiSearch';
+import { useSearchPrograms } from '../api/apiSearch';
 
-export const SearchComponent: React.FC<{ searchQuery: string }> = ({ searchQuery }) => {
-  // const [searchQuery, setSearchQuery] = useState('');
-  const { data: episodes, isLoading, isError } = useSearchEpisodes(searchQuery);
+export const SearchComponent: React.FC<{ searchQuery: string }> = ({ searchQuery }) => { 
+  // const { data: episode, isLoading, isError } = useSearchEpisodes(searchQuery);
+  const { data: programs, isLoading, isError } = useSearchPrograms(searchQuery);
 
-
-  // useEffect(() => {
-  //   const timerId = setTimeout(() => {
-  //     onSearch(searchQuery);
-  //   }, 500); // 500ms delay
-
-  //   return () => {
-  //     clearTimeout(timerId);
-  //   };
-  // }, [searchQuery, onSearch]);
-
-  // const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setSearchQuery(event.target.value);
-  // };
-
-  const handleEpisodeClick = (episode: IEpisode) => {
-    console.log(episode);
-    // Handle the episode click event here
-  };
-
-
-
-  if (isLoading) {
-    return <p>Loading...</p>;
+  function handleProgramClick(program: IProgram): void {
+    console.log(program);
   }
 
+  // function handleEpisodeClick(episode: IEpisode): void {
+  //   console.log(episode);
+  // }
+  console.log('Programs:', programs);
+  // console.log('Episodes:', episodes);
+
+  if (isLoading || !searchQuery) {
+    return null;
+  }
+
+
+
   if (isError) {
-    return <p>Error!</p>;
+    return <div>Error fetching data</div>;
   }
 
   return (
     <div>
-      {/* <input
-        type="text"
-        placeholder="Sök på Sveriges Radio..."
-        className="rounded py-2 px-4 text-gray-700 bg-white border-2 border-gray-200 focus:outline-none focus:bg-white focus:border-blue-500"
-        value={searchQuery}
-        onChange={handleSearchChange}
-      /> */}
       <div className="absolute bg-white rounded border mt-2 z-50 max-h-64 overflow-auto">
-        {episodes.map((episode: IEpisode) => (
-          <div key={episode.id} className="p-2 hover:bg-gray-200 cursor-pointer" onClick={() => handleEpisodeClick(episode)}>
-            {/* add small img right side */}
-            {/* <img src={episode.imageurl} alt={episode.title} className="float-left h-10 w-10" /> */}
-            {episode.title}
+        {/* {episodes.map((episode: IEpisode) => ( */}
+        {/* <div key={episode.id} className="p-2 hover:bg-gray-200 cursor-pointer" onClick={() => handleEpisodeClick(episode)}> */}
+        {/* {episode.title} */}
+        {/* {episode.title} */}
+        {/* </div> */}
+        {/* ))} */}
+        {programs.map((program: IProgram) => (
+          <div key={program.id} className="p-2 hover:bg-gray-200 cursor-pointer" onClick={() => handleProgramClick(program)}>
+            {program.name}
+            <div>
+              {/* <img src={program.programimagetemplate} alt="Program" /> */}
+
+            </div>
           </div>
         ))}
       </div>
