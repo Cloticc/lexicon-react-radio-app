a0fce9f2c7ad38a552ad4c0c80a80e40f4a7aab8
Andreas Isidorsson <isidorssona@gmail.com>
2024-03-05 11:00:38 +0100

    Add logging and implement program search API

--- src/api/apiSearch.tsx
+++ src/api/apiSearch.tsx
@@ -8,7 +8,10 @@ export const getSearchEpisodes = async (query: string) => {
     if (!response.ok) {
       throw new Error('Failed to fetch data');
     }
+
     const data = await response.json();
+    console.log('Data:', data);
+
     return data.episodes;
   } catch (error) {
     console.error('Error fetching data:', error);
@@ -16,9 +19,37 @@ export const getSearchEpisodes = async (query: string) => {
   }
 }
 
+
 export const useSearchEpisodes = (query: string) => {
   return useQuery({
     queryKey: ['searchEpisodes', query],
-    queryFn: () => getSearchEpisodes(query)
+    queryFn: () => getSearchEpisodes(query),
+    staleTime: 0,
+  });
+};
+
+
+export const getPrograms = async () => {
+  try {
+    const response = await fetch(`http://api.sr.se/api/v2/programs?format=json&size=1000`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const data = await response.json();
+    console.log('Data:', data);
+    
+    return data.programs;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+}
+
+export const useSearchPrograms = (name: string) => {
+  return useQuery({
+    queryKey: ['searchPrograms', name],
+    queryFn: () => getPrograms().then(programs => programs.filter((program: { name: string; }) => program.name.toLowerCase().includes(name.toLowerCase()))),
+    // staleTime: 0, //disable cache
   });
 };
