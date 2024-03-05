50cb6c0f592d53ed9f6af54981b5bace506f695e
Andreas Isidorsson <isidorssona@gmail.com>
2024-03-05 17:20:27 +0100

    Refactor Broadcasts component to use header element for each broadcast

--- src/components/Broadcasts.tsx
+++ src/components/Broadcasts.tsx
@@ -18,17 +18,16 @@ export const Broadcasts = () => {
 
   return (
     <div>
-      <header className='header-wrapper'>
-      </header>
+
       {broadcasts && broadcasts.map((broadcast: IBroadcast, index: number) => (
-        <div key={index} className="m-4 p-4 border rounded shadow-lg bg-white">
+        <header key={index} className="m-4 p-4 border rounded shadow-lg bg-white">
           <h2 className="text-2xl font-bold mb-2">{broadcast.name}</h2>
           <p className="text-gray-700">{broadcast.description}</p>
           <p>Start tid: {new Date(parseInt(broadcast.localstarttime.replace(/[^0-9 +]/g, ''))).toLocaleString('eu', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false })}</p>
           <p>Slut tid: {new Date(parseInt(broadcast.localstoptime.replace(/[^0-9 +]/g, ''))).toLocaleString('eu', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false })}</p>
           <p>Kanal: <a href={broadcast.liveaudio.url} target="_blank" rel="noopener noreferrer">{broadcast.channel.name}</a></p>
           <p>Utgivare: {broadcast.publisher.name}</p>
-        </div>
+        </header>
       ))}
     </div>
   );
