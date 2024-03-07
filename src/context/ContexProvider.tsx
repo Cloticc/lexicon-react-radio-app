3609474e604e63e1cbe9fb305af05910d4ea37ff
Andreas Isidorsson <isidorssona@gmail.com>
2024-03-07 18:14:48 +0100

    Refactor notification component in FavoriteProvider

--- src/context/ContexProvider.tsx
+++ src/context/ContexProvider.tsx
@@ -69,11 +69,34 @@ export function FavoriteProvider({ children }: IFavoriteProviderProps): ReactEle
   return (
     <FavoriteContext.Provider value={{ favorites, addFavorite, removeFavorite }}>
       {notification && (
-        <div className={`z-50 fixed bottom-10 right-0 m-6 p-4 rounded shadow-lg text-white ${notification.type === 'success' ? 'bg-green-500' : 'bg-red-500'}`}>
-          {notification.message}
-          <button onClick={() => setNotification(null)} className="absolute top-0 right-0 m-2">
-            X
-          </button>
+        <div role="alert" className="rounded-xl border border-gray-100 bg-white p-4 absolute bottom-4 right-4  h-32">
+          <div className="flex items-start gap-4">
+            <span className="text-green-600">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fillRule="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </span>
+
+            <div className="flex-1">
+              <strong className="block font-medium text-gray-900">{notification.type === 'success' ? 'Success' : 'Error'}</strong>
+              <p className="mt-1 text-sm text-gray-700">{notification.message}</p>
+            </div>
+
+            <button onClick={() => setNotification(null)} className="absolute top-0 right-0 m-2">
+              X
+            </button>
+          </div>
         </div>
       )}
       {children}
